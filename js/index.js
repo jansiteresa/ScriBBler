1b7009a2f38a7b2442185333309708b6bd305133
jansiteresa <jansiteresa94@gmail.com>
2020-10-04 18:37:34 +0530

    added forms for signin and signup

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function showSignInModal(){
+    var modal = document.getElementById("signInModal");
+    modal.style.display = "flex";
+  }
+
+  function hideSignInModal(){
+    var modal = document.getElementById("signInModal");
+    modal.style.display = "none";
+  }
+  function showSignUpModal(){
+    var modal = document.getElementById("signUpModal");
+    modal.style.display = "flex";
+  }
+  function hideSignUpModal(){
+    var modal = document.getElementById("signUpModal");
+    modal.style.display = "none";
+  }
